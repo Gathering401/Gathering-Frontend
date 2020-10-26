1d954b4ba7017ed77c8dcac73106bd0282b87835
Enrique Hilst <enrique.hilst@gmail.com>
2020-10-26 15:23:20 -0500

    Implementing Event page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import LoginPage from './components/LoginPage';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+//import Event from './components/Event';
 
 function App() {
   return (
@@ -17,8 +18,6 @@ function App() {
       </Row>
     </Container>
     </div>
-
-
   );
 }
 
