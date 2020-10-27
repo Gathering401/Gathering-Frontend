c9eae34a71a9f10eaff61638cf6aef931fbed9d7
Enrique Hilst <enrique.hilst@gmail.com>
2020-10-27 14:49:11 -0500

    Added Register modal

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,28 +3,31 @@ import LoginForm from './auth/LoginForm';
 import Header from './Header';
 import Button from 'react-bootstrap/Button';
 import { Modal } from 'react-bootstrap';
+import Register from './auth/RegisterForm';
 
 export default function Login() {
     const [show, setShow] = useState(false);
-
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-
-
 
     return (
         <>
             <Header />
             <LoginForm />
-            <Button onClick={handleShow}/>
+            <Button onClick={handleShow}>Click to Register</Button>
 
             <Modal
             show={show}
             onHide={handleClose}
-            backdropClassName="static"
-            keyboard= {false}
+            backdrop="static"
+            keyboard={false}
             >
-                
+                <Modal.Header closeButton>
+                <Modal.Title>Register a new user</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Register />
+                </Modal.Body>
             </Modal>
         </>
     );
