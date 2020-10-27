43cd1c16b53d60defc6273f28252260ece5341f3
Enrique Hilst <enrique.hilst@gmail.com>
2020-10-27 10:39:07 -0500

    Fixed routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Switch,Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home'
 import Login from './components/LoginPage';
 import Register from './components/auth/RegisterForm';
@@ -8,22 +8,19 @@ import Event from './components/Event';
 function App() {
   return (
     <div className="App">
-      
       <Switch>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <Route path="/Login" exact>
-            <Login />
-          </Route>
-          <Route path="/Register" exact>
-            <Register />
-          <Route path="/Event" exact>
-            <Event />
-          </Route>
-          <Route path="/Event" exact>
-            <Event />
-          </Route>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/Login" exact>
+          <Login />
+        </Route>
+        <Route path="/Register" exact>
+          <Register />
+        </Route>
+        <Route path="/Event" exact>
+          <Event />
+        </Route>
       </Switch>
     </div>
   );
