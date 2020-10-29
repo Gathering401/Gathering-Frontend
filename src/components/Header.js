c3caeacc346f3a1331681ee45fd6159c6b335f13
Enrique Hilst <enrique.hilst@gmail.com>
2020-10-29 17:28:45 -0500

    Added gathering animation to top link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import logo from '../logo.svg';
 import { Navbar } from 'react-bootstrap'
-import Nav from 'react-bootstrap/Nav';
+//import Nav from 'react-bootstrap/Nav';
+import '../Brand.css';
 
 export default function Header() {
   return (
@@ -15,14 +16,14 @@ export default function Header() {
             height="30"
             className="d-inline-block align-top"
           />{' '}
-          Gathering
+          <button className="gathering"><h1>Gathering</h1></button>
         </Navbar.Brand>
-        <Nav.Item>
+        {/* <Nav.Item>
           <Nav.Link eventKey="link-1">Link</Nav.Link>
         </Nav.Item>
         <Nav.Item>
           <Nav.Link eventKey="link-2">Link</Nav.Link>
-        </Nav.Item>
+        </Nav.Item> */}
       </Navbar>
     </>
   )
