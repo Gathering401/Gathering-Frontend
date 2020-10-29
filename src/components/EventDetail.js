6b96265212cb39901b1a9c71a18e7ebdf3546035
Enrique Hilst <enrique.hilst@gmail.com>
2020-10-28 19:08:50 -0500

    Added fetching to API for event details

--- src/components/EventDetail.js
+++ src/components/EventDetail.js
@@ -1,17 +1,30 @@
 import React, { useState } from 'react';
 import { Modal } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
+import { useAuth } from '../context/auth';
 
-export default function EventDetailsModal(props) {
+
+export default async function EventDetailsModal(props) {
+  const { user } = useAuth();
   const { eventId } = props;
 
-  console.log(eventId);
+  const eventAPI = `https://gathering.azurewebsites.net/api/Event/${eventId}`;
 
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   // await fetch from setShow = true 
+
+  const result = await fetch(`${eventAPI}`, {
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    },
+  });
+
+  const eventDetails = await result.json();
+
+  console.log(eventDetails);
 
 
   return (
