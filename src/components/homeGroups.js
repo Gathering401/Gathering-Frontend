abe29bb7ef265927540d48aed4104a837b9bf692
ChristianLievrouw <celievrouw1@gmail.com>
2020-10-29 13:13:32 -0500

    added styling

--- src/components/homeGroups.js
+++ src/components/homeGroups.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import {Card, Container,Col, Row} from 'react-bootstrap';
-import { Badge} from 'reactstrap';
+import { Card, Container, Col, Row } from 'react-bootstrap';
+import { Badge } from 'reactstrap';
 import '../App.css'
 import { useAuth } from '../context/auth';
 import Event from './Event';
@@ -11,62 +11,64 @@ import EventDetail from './EventDetail'
 export default function HomeGroups() {
   const { user } = useAuth();
   const userAPI = 'https://gathering.azurewebsites.net/api/Group';
-  const [groups,setGroups] = useState([]);
-  
+  const [groups, setGroups] = useState([]);
+
   useEffect(() => {
     async function getGroups() {
-      
-      const result = await fetch(`${userAPI}`,{
+
+      const result = await fetch(`${userAPI}`, {
         headers: {
           'Authorization': `Bearer ${user.token}`
         },
       });
       const resultBody = await result.json();
-      
+
       return setGroups(resultBody);
     }
     getGroups();
     // eslint-disable-next-line
-  },[user]);
-   
+  }, [user]);
+
   console.log(groups);
 
   return (
-    <>
+    <Row>
       {groups.map((group) => (
         <Card className="group-card">
           <Card.Body>
-            <Card.Title>{group.groupName}</Card.Title>
+            <Card.Title className="group-title"><h2>{group.groupName}</h2></Card.Title>
             <Container>
               <Card.Text>{group.description}</Card.Text>
-              <GroupEvent groupEvents={group.groupEvents}/>
+              <GroupEvent groupEvents={group.groupEvents} />
               <div className="event-stuff">
-              <Event />
+                <Event />
               </div>
             </Container>
           </Card.Body>
         </Card>
       ))}
-    </>
+    </Row>
   )
 }
 
 function GroupEvent(props) {
-  const {groupEvents} = props;
+  const { groupEvents } = props;
   return (
-    
     <Row>
       {groupEvents.map((event) => (
         <Col>
-          <Card>
+          <Card className="event-card">
             <Card.Title>{event.eventName}</Card.Title>
-            <Card.Text>
-              <Badge className="Button" color="success" pill>Status</Badge>
+            <Badge className="Button" color="success">Status</Badge>
+            <Card.Text className="event-button">
+              <br></br>
               <EventDetail eventId={event.eventId} />
             </Card.Text>
           </Card>
         </Col>
       ))}
     </Row>
+    
   )
 }
+
